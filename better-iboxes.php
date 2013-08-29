786003683986d964bf2de2c6d1fcdfa4c26eb417
Aleksander Hansson <aleksander.hansson@me.com>
2013-08-29 20:45:57 +0200

    Describtion fix

--- better-iboxes.php
+++ better-iboxes.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Better iBoxes
 	Demo: http://betteriboxes.ahansson.com
-	Description: An better way to create and configure several iBoxes at once. It is all about options!
+	Description: A better way to create and configure several iBoxes at once. It is all about options!
 	Version: 1.2
 	Author: Aleksander Hansson
 	Author URI: http://ahansson.com
